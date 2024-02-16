4ea7c28e5ec8fe4ac811bfb2f2feea159d76a9a3
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-02-16 13:49:57 -0300

    add basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io, process};
+
+fn main() -> io::Result<()> {
+    let mut args = env::args();
+    args.next().expect("Program not provided.");
+    if let Some(arg) = args.next() {
+        match arg.as_str() {
+            "help" => return usage(),
+            _ => {}
+        }
+        match compile_file(arg) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1)
+            }
+        }
+    } else {
+        usage()
+    }
+}
+
+fn compile_file(_arg: String) -> Result<String, String> {
+    todo!()
+}
+
+fn usage() -> io::Result<()> {
+    println!(
+        r###"
+        Usage: qbf <file.bf>
+        "###
+    );
+    Ok(())
 }
